cfd119789bd4ecdfe1745b529165228aac002ad6
Ethan Nielsen <ecnielsen11@byui.edu>
2024-03-29 15:33:34 -0600

    updated validation

--- utilities/index.js
+++ utilities/index.js
@@ -78,23 +78,23 @@ Util.buildDetailGrid = async function(data){
 }
 
 Util.buildClassificationList = async function (classification_id = null) {
-  let data = await invModel.getClassifications()
+  let data = await invModel.getClassifications();
   let classificationList =
-    '<select name="classification_id" id="classificationList" required>'
-  classificationList += "<option value=''>Choose a Classification</option>"
+    '<select name="classification_id" id="classificationList">';
+  classificationList += "<option>Choose a Classification</option>";
   data.rows.forEach((row) => {
-    classificationList += '<option value="' + row.classification_id + '"'
+    classificationList += '<option value="' + row.classification_id + '"';
     if (
       classification_id != null &&
       row.classification_id == classification_id
     ) {
-      classificationList += " selected "
+      classificationList += " selected ";
     }
-    classificationList += ">" + row.classification_name + "</option>"
-  })
-  classificationList += "</select>"
-  return classificationList
-}
+    classificationList += ">" + row.classification_name + "</option>";
+  });
+  classificationList += "</select>";
+  return classificationList;
+};
 
 /* ****************************************
  * Middleware For Handling Errors
--- utilities/inventory-validation.js
+++ utilities/inventory-validation.js
@@ -74,7 +74,18 @@ validate.inventoryRule = () => {
 };
 
 validate.checkInventoryData = async (req, res, next) => {
-  let classificationList = await utilities.buildClassificationList();
+  const {  inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id } = req.body;
+ 
+  let classificationList = await utilities.buildClassificationList(classification_id);
   let errors = [];
   errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -84,6 +95,15 @@ validate.checkInventoryData = async (req, res, next) => {
       title: "Add Inventory",
       nav,
       classificationList,
+      inv_make,
+      inv_model,
+      inv_year,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_miles,
+      inv_color
     });
     return;
   }
